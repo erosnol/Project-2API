8264043d0e39e2360c31cd021be0e6201f8c4d2b
Eros Nolasco-Mendoza <erosnolasco@me.com>
2022-06-17 10:18:28 -0400

    heruko

--- Project2API/server.js
+++ Project2API/server.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
+    console.log(`Server is running on port: `);
     mongoConfig()
 })
